bad04af7061d6d880134bab58acb36a8c80492b5
Yuriy Makarevich <makarevichy@gmail.com>
2018-05-27 20:48:40 +0000

    Day 8. Analog waffle function

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Week_2_100DaysOfCode
-Code of the second week of the challenge
+# Week 2
+
+### Day 8: May 27, Sunday.
+
+**Today's Progress**: Wrote analog waffle function.
+
+**Thoughts** The second week of the challenge began. Wrote analog functions waffle. The display options still need manual adjustment. You can see in the pictures that came out of this on the example of the numerical composition of the US Congress and the Russian Parliament.
+
+**Link(s) to work**
+[Code on Github](https://github.com/makarevichy/Week_1_100DaysOfCode/tree/master/weekday/Day_8_waffle_function.R)
---
+++ weekday/Day_8_waffle_function.R
@@ -0,0 +1,59 @@
+waf <- function(val, n_row = NULL, legend, main = NULL) {
+  require(RColorBrewer)
+  if (!is.numeric(val)) 
+    stop("'val' must be numeric vector", call. = FALSE)
+  if (is.null(n_row)) {
+    n_row <- 10
+  }
+  n_col <- ceiling(sum(val)/n_row)
+  x <- 0:(n_col + 2)
+  y <- 0:(n_row + 2)
+  co <- vector("character", 0L)
+  col_vec <- brewer.pal(8, name = "Set2")
+  for (i in seq_along(val)) {
+    y <- rep(col_vec[i], val[i])
+    co <- c(co, y)
+  }
+  col_matrix <- matrix(c(co, rep("white", n_row - (sum(val)%%n_row))), 
+                       ncol = n_row)
+  # plot
+  op <- par(bty = "n", xaxt = "n", yaxt = "n")
+  if (dim(col_matrix)[2] > dim(col_matrix)[1]) {
+    plot(1:(dim(col_matrix)[2] + 1), 1:(dim(col_matrix)[2] + 1), type = "n", 
+         xlab = "", ylab = "", main = main)
+    legend("top", border = "white", legend = legend, bty = "n", horiz = T, fill = unlist(unique(as.list(col_matrix))))
+    for (j in 0:(dim(col_matrix)[2] - 1)) {
+      rect(xleft = j + 1, ybottom = 1, xright = j + 1.95, ytop = 1.95, 
+           border = "white")
+      for (i in 1:(dim(col_matrix)[1])) {
+        rect(xleft = 1 + j, ybottom = i, xright = 1 + 0.95 + j, 
+             ytop = i + 0.95, col = col_matrix[i, j + 1], border = "white")
+      }
+    }
+  } else {
+    plot(1:(dim(col_matrix)[1] + 1), 1:(dim(col_matrix)[1] + 1), type = "n", 
+         xlab = "", ylab = "", main = main)
+    legend("right", border = "white", legend = legend, bty = "n", fill = unlist(unique(as.list(col_matrix))))
+    for (j in 0:(dim(col_matrix)[2] - 1)) {
+      rect(xleft = j + 1, ybottom = 1, xright = j + 1.95, ytop = 1.95, 
+           border = "white")
+      for (i in 1:(dim(col_matrix)[1])) {
+        rect(xleft = 1 + j, ybottom = i, xright = 1 + 0.95 + j, 
+             ytop = i + 0.95, col = col_matrix[i, j + 1], border = "white")
+      }
+    }
+  }
+  par(op)
+}
+
+jpeg('img/russian_parliament.jpg', width = 360, height = 360)
+waf(c(343, 42, 39, 23, 2, 4), n_row = 15, 
+    legend = c('United Russia', 'Communist Party', 'Liberal-democratic', 'Just Russia', 
+               'Independent', 'Vacant'),
+    main = 'Russian parliament')
+dev.off()
+jpeg('img/usa_congress.jpg', width = 360, height = 360)
+waf(c(51, 47, 2), n_row = 13, 
+    legend = c('Republican', 'Democratic', 'Independent'),
+    main = 'United States Congress')
+dev.off()
